a1805c82382441d66f41f2d6eb82a1e465686283
suryaoka21 <suryaokach@gmail.com>
2020-10-09 10:39:04 +0700

    a

---
+++ Sc Kirim Email Sederhana/Main.py
@@ -0,0 +1,63 @@
+# Kirim Email tanpa lampiran
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+fromaddr = "ALAMAT EMAIL PENGIRIM"
+toaddr = "ALAMAT EMAIL PENERIMA"
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "JUDUL PESAN"
+ 
+body = "ISI PESAN"
+msg.attach(MIMEText(body, 'plain'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "PASSWORD EMAIL PENGIRIM")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
+print("====Email Berhasil Terkirim Borrr====")
+
+
+# Kirim Email dengan Lampiran
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+fromaddr = "ALAMAT EMAIL PENGIRIM"
+toaddr = "ALAMAT EMAIL PENERIMA"
+ 
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "JUDUL PESAN"
+ 
+body = "ISI PESAN"
+ 
+msg.attach(MIMEText(body, 'plain'))
+
+'''
+Lampiran, sesuaikan nama filename dengan nama di attachment
+'''
+filename = "NAMA FILE"
+attachment = open("PATH KE FILE", "rb")
+ 
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+ 
+msg.attach(part)
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "PASSWORD EMAIL PENGIRIM")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
+print("====Email Berhasil Terkirim Borrr====")
